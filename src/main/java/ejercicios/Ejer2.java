13a21ac13b4234d65434b792d3dee714284ba666
Noelia <perezgonzaleznoelia08@gmail.com>
2022-12-09 11:15:20 +0100

    corregido

--- src/main/java/ejercicios/Ejer2.java
+++ src/main/java/ejercicios/Ejer2.java
@@ -13,31 +13,31 @@ import java.util.Random;
 public class Ejer2 {
 
     /**
-     * Rellenar un array char desde 'A' a 'Z' (mayúsculas)
-     * Generar un tamaño aleatorio entre 15 y 25
-     * Crear un array de ese tamaño y rellenarlo con posiciones válidas aleatorias
+     * Rellenar un array char desde 'A' a 'Z' (mayúsculas) Generar un tamaño
+     * aleatorio entre 15 y 25 Crear un array de ese tamaño y rellenarlo con
+     * posiciones válidas aleatorias
      */
     public static void main(String[] args) {
-        
+
         final int TAMAÑO = 26;
-        
+
         char[] letras = new char[TAMAÑO];
-        
+
         for (int i = 0; i < letras.length; i++) {
-            letras[i] = (char)(65 + i);
+            letras[i] = (char) (65 + i);
             System.out.print(letras[i]);
         }
         System.out.println();
-        
+
         Random aleatorio = new Random();
         int tamañoAleatorio = aleatorio.nextInt(11) + 15;
         char[] letras2 = new char[tamañoAleatorio];
         for (int i = 0; i < letras2.length; i++) {
             letras2[i] = letras[aleatorio.nextInt(letras.length)];
-            
             System.out.print(letras2[i]);
         }
-        
+        //había que generar varios string, no solo uno, pidiendose por teclado
+
     }
-    
+
 }
